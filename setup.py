c91a6ab03eb2395155c99e150c6504a73cbaa3cb
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2021-01-29 15:54:17 -0500

    Bump to version 0.0.5->0.0.6

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name='photostore',
-    version='0.0.5',
+    version='0.0.6',
     url='https://github.com/ybenitezf/photostore',
     license='GPL',
     author='Yoel Benítez Fonseca',
