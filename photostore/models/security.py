054af703dd80a23cdfc868dc87175a50b5cda339
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2021-02-03 15:30:04 -0500

    Adds credit_line to user model

--- photostore/models/security.py
+++ photostore/models/security.py
@@ -80,6 +80,7 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(120))
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(254), index=True)
+    credit_line = db.Column(db.String(254))
     password_hash = db.Column(db.String(128))
     roles = db.relationship(
         'Role', secondary=user_roles, lazy='select', 
